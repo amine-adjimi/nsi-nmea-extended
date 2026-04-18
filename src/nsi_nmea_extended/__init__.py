d3afc8c82800df3123f8442f731d8331508d1a1d
Amine Adjimi <poulpe3324@gmail.com>
2026-04-18 23:45:01 +0200

    Fixed some import errors

--- src/nsi_nmea_extended/__init__.py
+++ src/nsi_nmea_extended/__init__.py
@@ -1,4 +1,4 @@
-from check import check_both, check_value, check_type
-from ansi import ansi
-from nmea_sentence import NmeaSentence
-from nmea_dataset import NmeaDataset
+from .check import check_both, check_value, check_type
+from .ansi import ansi
+from .nmea_sentence import NmeaSentence
+from .nmea_dataset import NmeaDataset
